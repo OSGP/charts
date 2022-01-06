123a2b5f0489fecd1594e2b2d8b186059e41a1c8
Marcel Jansen <marcel.jansen@alliander.com>
2022-01-06 15:10:50 +0100

    change kubeval to kubeconform

---
+++ .github/kubeconform.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+KUBECONFORM_VERSION="v0.4.12"
+SCHEMA_LOCATION="https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/"
+
+# install kubeconform
+curl --silent --show-error --fail --location --output /tmp/kubeconform.tar.gz https://github.com/yannh/kubeconform/releases/download/"${KUBECONFORM_VERSION}"/kubeconform-linux-amd64.tar.gz
+tar -xf /tmp/kubeconform.tar.gz kubeconform
+
+# validate charts
+for CHART_DIR in ${CHART_DIRS}; do
+  helm dependency build "${CHART_DIR}"
+  helm template "${CHART_DIR}" | ./kubeconform -strict -ignore-missing-schemas -kubernetes-version "${KUBERNETES_VERSION#v}" -schema-location "${SCHEMA_LOCATION}" -verbose -summary
+done
